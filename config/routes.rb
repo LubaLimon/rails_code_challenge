b4bd9ea171d7cef9d6f35f1e894511724e3f889e
Lubneuski Aliaksandr <lubagabit@gmail.com>
2020-08-01 02:48:26 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'admin#index'
 
-  resources :orders, only: [:index, :show], param: :number
-
+  resources :orders, only: [:index, :show, :update], param: :number
+ 
   resources :reports, only: :index do
+    collection do
+      get :coupon_users
+      get :sales_by_product
+    end
   end
 end
