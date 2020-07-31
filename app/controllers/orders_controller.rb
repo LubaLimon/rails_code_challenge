d4ec923603bb4d0f420d442de3910aee81c0e454
Lubneuski Aliaksandr <lubagabit@gmail.com>
2020-07-31 20:39:34 +0300

    Refactor orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
   before_action :find_order!, only: [:show, :update]
 
+  SEARCHABLE_FIELDS = ['orders.number','orders.state', 'users.email'].freeze
+
   def index
     if params[:query].present?
-      @orders ||= Order.joins([:user, :address]).where("#{params[:query_field]} LIKE ?", "%#{params[:query]}%").paginate(page: params[:page])
+      @orders ||= Order.joins([:user, :address]).where("#{query_field} LIKE ?", "%#{params[:query]}%").paginate(page: params[:page])
     else
       @orders ||= Order.joins([:user, :address]).paginate(page: params[:page])
     end
@@ -16,8 +18,17 @@ class OrdersController < ApplicationController
     @order.cancel
     redirect_to  action: :show
   end
+
   private
   def find_order!
     @order = Order.find_by!(number: params[:number])
+  end
+
+  def query_field
+    if SEARCHABLE_FIELDS.include? params[:query_field]
+      params[:query_field]
+    else
+      nil
+    end
   end
 end
